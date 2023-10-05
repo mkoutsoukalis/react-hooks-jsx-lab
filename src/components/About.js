e4b6386d3bb94bd8610859c95f266f9e3f7c915f
Maria Koutsoukalis <mkoutsoukalis@outlook.com>
2023-10-04 20:27:21 -0400

    Completed

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>Getting started with React has been a total blast, I can't wait to dive even deeper!</p>
+    <img src={image} alt="I made this"></img>
+  </div>
+  )
 }
 
 export default About;
